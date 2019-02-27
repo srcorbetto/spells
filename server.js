f99412a7974c915b327004d211e66581aa73660e
Mike Corbett <srcorbetto@gmail.com>
2019-02-26 19:33:32 -0600

    require order fix + npm start

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 require('dotenv').config({path: __dirname + '/.env'});
 
-const app = express();
 const bodyParser = require('body-parser');
 const express = require('express');
+const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true}));
